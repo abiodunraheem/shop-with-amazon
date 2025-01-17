087ea960f0ad28a07ded0c478fd23e6bf3dfc6ac
unknown <raheemabiodun23@gmail.com>
2025-01-17 13:25:09 +0100

    Add checkout page and fix and fic price fomattinng

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,8 @@
-let productsHTML ='';
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
+let productsHTML ='';
 products.forEach((product) => {
     productsHTML += `
         <div class="product-container">
@@ -21,7 +24,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
@@ -58,28 +61,21 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+function updateCartQuantity () {
+  let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    })
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
-    const productId = button.dataset.productId;
+    const productId = (button.dataset.productId);
+    addToCart (productId)
+    updateCartQuantity ()
 
-    let matchingItem;
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-          matchingItem = item;
-      }
-    })
-
-    if (matchingItem) {
-      matchingItem.quantity +=1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1
-      })
-    }
-
-    
     console.log(cart)
   })
 })
-
