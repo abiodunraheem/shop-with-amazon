682aae2706bdd9ebe0f50bd13db7ce9e7f609e06
unknown <raheemabiodun23@gmail.com>
2025-01-18 21:34:18 +0100

    Add delete functionality to delete cart item from html page

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -14,11 +14,9 @@ cart.forEach((cartItem) => {
         }
     });
 
-    console.log(matchingProduct)
-
     cartSummaryHTML += 
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -41,8 +39,9 @@ cart.forEach((cartItem) => {
                 <span class="update-quantity-link link-primary">
                 Update
                 </span>
-                <span class="delete-quantity-link link-primary">
-                Delete
+                <span class="delete-quantity-link link-primary js-delete-link" 
+                    data-product-id = "${matchingProduct.id}">
+                    Delete
                 </span>
             </div>
             </div>
@@ -94,7 +93,15 @@ cart.forEach((cartItem) => {
         </div>
     </div>
     `
-    console.log(matchingProduct)
 })
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+    link.addEventListener('click', () => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId)
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove()
+    })
+})
